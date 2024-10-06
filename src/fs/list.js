ef576cf94096abe21e2266816a7acf3d6f6b9063
Keryu7 <yyurikt@tut.by>
2024-10-06 17:43:10 +0200

    feat: added list.js task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
 const list = async () => {
-    // Write your code here 
+    const dirPath = path.join(process.cwd(), 'files');
+
+    try {
+        await fs.access(dirPath);
+        const files = await fs.readdir(dirPath);
+        files.forEach(file => {
+            console.log(file);
+        });
+
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
