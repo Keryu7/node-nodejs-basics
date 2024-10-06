7a49d26a5e4893b1c4ed225ca8ec12ee7d05ad39
Keryu7 <yyurikt@tut.by>
2024-10-06 17:15:29 +0200

    feat: added copy.js task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+    const sourceDir = path.join(process.cwd(), 'files');
+    const destDir = path.join(process.cwd(), 'files_copy');
+
+    try {
+        await fs.access(sourceDir);
+        try {
+            await fs.access(destDir);
+            throw new Error('FS operation failed');
+        } catch (error) {
+            if (error.code !== 'ENOENT') {
+                throw error;
+            }
+        }
+        await fs.cp(sourceDir, destDir, { recursive: true });
+        console.log('Successfully copied folder');
+
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
-await copy();
+copy();
