6ddbef94a20e92694f277f8e35ba6267f56e398d
Keryu7 <yyurikt@tut.by>
2024-10-06 17:25:52 +0200

    feat: added rename.js task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
-const rename = async () => {
-    // Write your code here 
+import { promises as fs } from 'fs';
+import path from 'path';
+
+const renameFile = async () => {
+    const wrongFilePath = path.join(process.cwd(), 'files', 'wrongFilename.txt');
+    const properFilePath = path.join(process.cwd(), 'files', 'properFilename.md');
+
+    try {
+        await fs.access(wrongFilePath);
+        try {
+            await fs.access(properFilePath);
+            throw new Error('FS operation failed');
+        } catch (error) {
+            if (error.code !== 'ENOENT') {
+                throw error;
+            }
+        }
+
+        await fs.rename(wrongFilePath, properFilePath);
+        console.log('File renamed successfully');
+
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
-await rename();
+renameFile();
