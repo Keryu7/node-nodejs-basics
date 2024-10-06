498682a7e409c19e8647ed0a7a0be6e039d79239
Keryu7 <yyurikt@tut.by>
2024-10-06 16:56:08 +0200

    init: start nodejs-basics

---
+++ src/fs/create.js
@@ -0,0 +1,21 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+const create = async () => {
+    const filePath = path.join(process.cwd(), 'files', 'fresh.txt');
+    const content = 'I am fresh and young';
+
+    try {
+        await fs.access(filePath);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            await fs.writeFile(filePath, content, 'utf8');
+            console.log('File created successfully');
+        } else {
+            throw new Error('FS operation failed');
+        }
+    }
+};
+
+create();
