b20c78d142cd179a079d8515c81322f03668fb6e
Keryu7 <yyurikt@tut.by>
2024-10-06 18:23:13 +0200

    feat: added args.js env.js tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = '';
+    const envVariables = Object.entries(process.env)
+        .filter(([key]) => key.startsWith(prefix))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+
+    if (envVariables) {
+        console.log(envVariables);
+    } else {
+        console.log(`No ${prefix} environment variables found.`);
+    }
 };
 
 parseEnv();
