660bedf04ca72ecce88321929afe24c92661dbea
Keryu7 <yyurikt@tut.by>
2024-10-06 19:24:34 +0200

    feat: added calcHash.js task (Hash)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import crypto from 'crypto';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(process.cwd(), 'files', 'fileToCalculateHashFor.txt');
+    const hash = crypto.createHash('sha256');
+    const stream = fs.createReadStream(filePath);
+
+    stream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+    stream.on('end', () => {
+        const result = hash.digest('hex');
+        console.log(`SHA256 hash for file ${filePath}: ${result}`);
+    });
+    stream.on('error', (error) => {
+        console.error(`Error while reading file: ${error.message}`);
+    });
 };
 
 await calculateHash();
