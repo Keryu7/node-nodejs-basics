4fb5174c8eb9c042697887dc50b6fa2aa2b7d168
Keryu7 <yyurikt@tut.by>
2024-10-07 19:30:31 +0200

    feat: added Worker Threads tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const numCPUs = os.cpus().length;
+    let number = 10;
+    const results = [];
+    const promises = [];
+
+    for (let i = 0; i < numCPUs; i++) {
+        promises.push(new Promise((resolve) => {
+            const worker = new Worker('./worker.js');
+            worker.postMessage(number);
+            worker.on('message', (result) => {
+                results[i] = result;
+                resolve();
+            });
+            worker.on('error', () => {
+                results[i] = { status: 'error', data: null };
+                resolve();
+            });
+            number++;
+        }));
+    }
+    await Promise.all(promises);
+    console.log(results);
 };
 
 await performCalculations();
